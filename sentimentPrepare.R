f617b56371cc3129b2cad12c5acd58ea08ca7ed2
thor <thor@cphbusiness.dk>
2022-06-04 09:15:46 +0200

    sentiment prepare

---
+++ sentimentPrepare.R
@@ -0,0 +1,25 @@
+library(dplyr)
+library(Sentida)
+library(stringr)
+library(readr)
+library(mongolite)
+library(ggplot2)
+
+
+# OPG1: Forbind til databasen "edc" og collection "vil7"
+conedc <-mongo(
+  url = "mongodb://46.101.144.32",
+  verbose = TRUE
+)
+
+# OPG2: Hent vil7 ind i R og beregn sentida-scoren på 10 rækker
+
+# OPG3: Beregn på alle og find de 10 mest positive og 10 mest negative
+#       Plot score mod sqmprice
+
+
+# OPG4: Lav et tjek på sqmprice ud fra area og price
+
+
+
+
